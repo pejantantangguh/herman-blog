e22af57925304020f52f7eb31a78d98ab313ef9e
pejantantangguh <suherman.ng@gmail.com>
2019-07-29 21:14:15 +1000

    Removed .cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/suhermanng/Programming/myblog/node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/suhermanng/Programming/myblog/node_modules/gatsby-mdx/gatsby-browser.js'),
-      options: {"plugins":[],"defaultLayouts":{"default":"/Users/suhermanng/Programming/myblog/src/components/layout.js"},"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-images","options":{"maxWidth":880,"linkImagesToOriginal":false}}]},
-    }]
